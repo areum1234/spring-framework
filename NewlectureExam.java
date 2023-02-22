ea7753237e4ad784ee8f25e322d7bfabecfa2926
areum1234 <armang252@gamil.com>
2023-02-22 13:40:59 +0900

    커밋 테스트

---
+++ NewlectureExam.java
@@ -0,0 +1,75 @@
+package com.newlecture.spring.di.entity;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewlectureExam implements Exam {
+
+	@Value("20")
+	private int math;
+	private int eng;
+	private int kor;
+	private int com;
+	
+	public NewlectureExam() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public NewlectureExam(int math, int eng, int kor, int com) {
+		super();
+		this.math = math;
+		this.eng = eng;
+		this.kor = kor;
+		this.com = com;
+	}
+
+	@Override
+	public int total() {
+		return math+eng+kor+com;
+	}
+
+	@Override
+	public float avg() {
+		return total() / 4.0f;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+
+	@Override
+	public String toString() {
+		return "NewlectureExam [math=" + math + ", eng=" + eng + ", kor=" + kor + ", com=" + com + "]";
+	}
+
+	
+}
